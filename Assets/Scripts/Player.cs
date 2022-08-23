9803397566ee0989e27e5035b16a7c230d6f422a
Grant Kennedy <grantk819@gmail.com>
2022-08-23 11:27:16 -0700

    player occlusion work
    switched to the rendering layer feature model instead of custom shaders. Backing up before changing all materials to Forward Renderer to test.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,6 @@ public class Player : MonoBehaviour
     Text_Manager textManager;
     Scene_Manager sm;
 
-    SpriteRenderer _double;
 
     [SerializeField]
     protected float baseSpeed;
@@ -143,7 +142,6 @@ public class Player : MonoBehaviour
 
         audioSource = this.GetComponent<AudioSource>();
 
-        _double = this.transform.Find("double").GetComponent<SpriteRenderer>();
         
 
     }
@@ -171,14 +169,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _double.sprite = this.spriteRenderer.sprite;
         AnimationUpdate();
     }
 
-    private void LateUpdate()
-    {
-        _double.sprite = this.spriteRenderer.sprite;
-    }
 
     private void FixedUpdate()
     {         
