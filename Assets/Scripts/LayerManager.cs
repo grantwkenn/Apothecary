0d089d226be1fca24663e8913e13a6ece8a1b033
Grant Kennedy <grantk819@gmail.com>
2023-02-03 13:40:24 -0800

    minor changes to prefabs
    Modified the Beach Tree Prefab, new class to separate the Dynamically layered objects from layer helper used in the editor.

--- Assets/Scripts/LayerManager.cs
+++ Assets/Scripts/LayerManager.cs
@@ -10,7 +10,7 @@ public class LayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        relayer();
+        //relayer();
     }
 
     void relayer()
